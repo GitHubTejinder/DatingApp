1f902284138f8949d4bec31e19d579dfbc98bb47
SINGH Tejinder <tejinder.singh@soprasteria.com>
2020-09-29 21:24:04 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _dataContext.Values.ToListAsync();
+            return Ok(result);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _dataContext.Values.FirstOrDefaultAsync(v => v.Id == id);
+            return Ok(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
