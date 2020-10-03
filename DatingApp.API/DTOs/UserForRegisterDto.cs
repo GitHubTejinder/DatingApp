410699723d9233d7a5ff11e5267d49717233a57d
SINGH Tejinder <tejinder.singh@soprasteria.com>
2020-10-03 16:50:01 +0530

    End of Section 3

---
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.DTOs
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(maximumLength:8, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 8 characters.")]
+        public string Password { get; set; }
+    }
+}
